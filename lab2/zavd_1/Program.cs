4b5c50dd3a3266da0fd12c89ec6b8d663a8275ce
SaganMykola <93724192+SaganMykola@users.noreply.github.com>
2022-05-17 22:26:05 +0300

    Add files via upload

---
+++ lab2/zavd_1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static double[] inputArray(int length)
+        {
+
+            double[] a = new double[length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                var rnd = new Random();
+                a[i] = rnd.Next(-10, 10);
+
+            }
+            return a;
+        }
+
+        static void printArr(double[] a)
+        {
+            foreach (var el in a)
+            {
+                Console.Write($"{el} ");
+            }
+            Console.WriteLine();
+        }
+
+        static double dovzVectora(double[] a)
+        {
+            double suma = Math.Pow(a[0], 2);
+            for (int i = 1; i < a.Length; i++)
+            {
+                suma += Math.Pow(a[0 + i], 2);
+            }
+            suma = Math.Sqrt(suma);
+            return suma;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double[] vec1 = inputArray(n);
+            double[] vec2 = inputArray(n);
+            double[] div = new double[n];
+            for (int i = 0; i < n; i++)
+                div[i] = vec1[i] - vec2[i];
+            double dovz = dovzVectora(div);
+            Console.Write("Перший вектор: ");
+            printArr(vec1);
+            Console.Write("Другий вектор: ");
+            printArr(vec2);
+            Console.Write("Рiзниця векторiв: ");
+            printArr(div);
+            Console.WriteLine($"Довжина вектора= {dovz}");
+        }
+    }
+}
---
+++ lab2/zavd_2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ConsoleApp18
+{
+
+    class Program
+    {
+
+        static double getAvarage(double a, double b)
+        {
+            double avarage = (a + b) / 2;
+            return avarage;
+        }
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            Console.Write("n= ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("m= ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            double[,] matrix = new double[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = rnd.Next(-10, 10);
+                }
+            }
+            Console.WriteLine("Матриця:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"   {matrix[i, j]}");
+                }
+                Console.WriteLine();
+            }
+            double max = matrix[0, 0];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                    if (matrix[i, j] > max)
+                        max = matrix[i, j];
+            }
+            Console.WriteLine($"Максимальний елемент= {max}");
+
+            double min = matrix[0, 0];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                    if (matrix[i, j] < min)
+                        min = matrix[i, j];
+            }
+            Console.WriteLine($"Мiнiмальний елемент= {min}");
+
+            double avarage = getAvarage(max, min);
+            Console.WriteLine($"середнє арифметичне найбiльшого i найменшого значень елементiв матрицi= {avarage}");
+        }
+    }
+}
