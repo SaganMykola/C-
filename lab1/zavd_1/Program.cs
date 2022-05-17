9b259383b283174755fba1efd30e07d206b052d8
SaganMykola <93724192+SaganMykola@users.noreply.github.com>
2022-05-17 22:25:08 +0300

    Add files via upload

---
+++ lab1/zavd_1/Program.cs
@@ -0,0 +1,49 @@
+﻿﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class zavd_1_Sagan
+    {
+
+        static double getMax(double a, double b)
+        {
+            if (a > b)
+                return a;
+
+            else
+                return b;
+        }
+
+        static double getMul(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double getDiv(double a, double b)
+        {
+            return a / b;
+        }
+
+
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+
+            var a = rnd.Next(-10, 10);
+            Console.WriteLine($"a= {a}");
+
+            var b = rnd.Next(-10, 10);
+            Console.WriteLine($"b= {b}");
+
+            var c = rnd.Next(-10, 10);
+            Console.WriteLine($"c= {c}");
+
+            var max1 = getMax(a - b, a + b);
+            Console.WriteLine(max1);
+            var max2 = getMax((a * b) / c, c / (a * b));
+            Console.WriteLine(max2);
+            var y = getMax(max1, max2);
+            Console.WriteLine($"y={y}");
+        }
+    }
+}
---
+++ lab1/zavd_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var geom_mean = WhileNumber();
+            Console.WriteLine(geom_mean);
+        }
+
+
+        static double WhileNumber()
+        {
+            var rnd = new Random();
+            var number = rnd.Next(1000, 9999);
+            Console.WriteLine(number);
+            var k = 0;
+            int max = 0;
+            int min = 10;
+            double geom_mean = 0;
+            while (number > 0)
+            {
+                k = number % 10;
+                if (k > max)
+                    max = k;
+
+                if (k < min)
+                    min = k;
+
+                number = number / 10;
+            }
+            Console.WriteLine($"Max={max}");
+            Console.WriteLine($"Min={min}");
+            geom_mean = Math.Sqrt(min * max);
+            return geom_mean;
+
+        }
+    }
+}
